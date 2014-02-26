f43786e70b7346076a90007a31c9f82e4c633167
Melinda Morris <mitzysama@icloud.com>
2014-02-26 16:07:55 -0500

    configure tracking helper to display minutes or hours

--- app/helpers/tracking_helper.rb
+++ app/helpers/tracking_helper.rb
@@ -1,9 +1,15 @@
 module TrackingHelper
   def duration(time_clock)
     if time_clock.ended
-      "#{number_with_precision(((time_clock.ended - time_clock.started)/ 1.hour).to_f, precision: 2)} hours"
+      hour_test = ((time_clock.ended - time_clock.started) / 60).to_i
+      if hour_test > 60
+        "#{number_with_precision(((time_clock.ended - time_clock.started)/ 1.hour).to_f, precision: 2)} hours"
+      else
+        "#{number_with_precision(((time_clock.ended - time_clock.started)/ 1.minute).to_f, precision: 2)} minutes"
+      end
+
     else
-      nil
+      "Active Timer Running"
     end
   end
 end
